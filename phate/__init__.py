c1c0ec27c868423bc85763d893c2a02304a251eb
Michael Welsch <michael@welsch.one>
2023-05-12 11:11:12 +0200

    Update __init__.py

--- phate/__init__.py
+++ phate/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 
 from .phate import PHATE
+from .phateAI import PHATEAE
 import phate.tree
 import phate.io
 import phate.preprocessing
 import phate.mds
 import phate.vne
 import phate.cluster
+import phate.phateAE
 from .version import __version__
